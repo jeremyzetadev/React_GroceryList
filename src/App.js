9631b4886f20b6ce1bfce6902fefaae6d7187241
jeremy zeta <jeremy_zeta@yahoo.com>
2025-04-22 23:30:57 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Header from './Header';
+import Footer from './Footer';
+import Content2 from './Content2';
+
+function App() {
+
+  return (
+    <div className="App">
+      <Header/>
+      <Content2/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
